4b35e6a1b9d4495178292323a16b06001432683c
guillaume <guillaume.dujardin@hotmail.fr>
2019-07-05 12:04:15 +0200

    code changed

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-#Fonction d'affichage de notre labyrinthe
-def affiche_labyrinthe(labyrinthe) :
-    for ligne in labyrinthe :
-        print(ligne)
+from Map.py import *
+from Perso.py import *
 
+#Message d'introduction au jeu
 
-#Definir (x) comme étant notre personnage
+message_debut = "Vous incarner Mcgiver dans un labyrinthe ! Aider le � sortir indemne"
+message_debut_modifier = message_debut.center(120,"*")
+print(message_debut_modifier)
+
 
 affiche_labyrinthe(labyrinthe) #afficher le labyrinthe
+
+print("Vous �tes X et devez rejoindre la sortie ! Bonne chance...\n")
