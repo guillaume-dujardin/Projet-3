cf1824f50f20d11712083cc49869a421a4995e9a
guillaume <guillaume.dujardin@hotmail.fr>
2019-07-05 11:50:03 +0200

    add

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello World ! ")
+#Fonction d'affichage de notre labyrinthe
+def affiche_labyrinthe(labyrinthe) :
+    for ligne in labyrinthe :
+        print(ligne)
+
+
+#Definir (x) comme étant notre personnage
+
+affiche_labyrinthe(labyrinthe) #afficher le labyrinthe
