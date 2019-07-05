59630e6524dd307fe1255bb9f94cae76dedf2802
guillaume <guillaume.dujardin@hotmail.fr>
2019-07-05 12:47:12 +0200

    Modif

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+from Map import *
+from Perso import *
+
 #Message d'introduction au jeu
 
-message_debut = "Vous incarner Mcgiver dans un labyrinthe ! Aider le � sortir indemne"
+message_debut = "Vous incarner Mcgiver dans un labyrinthe ! Aider le à sortir indemne"
 message_debut_modifier = message_debut.center(120,"*")
 print(message_debut_modifier)
 
 
 affiche_labyrinthe(labyrinthe) #afficher le labyrinthe
 
-print("Vous �tes X et devez rejoindre la sortie ! Bonne chance...\n")
+print("Vous êtes X et devez rejoindre la sortie ! Bonne chance...\n")
