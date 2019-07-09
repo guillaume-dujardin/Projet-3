ab765ad231aac17c4d264258aafc7f2d562ca7be
guillaume <guillaume.dujardin@hotmail.fr>
2019-07-09 23:21:46 +0200

    Add function (down left up)

--- Character.py
+++ Character.py
@@ -16,9 +16,23 @@ class Character():
         return map
 
     def move_left(self, map):
+        if map.map_array[self.y][self.x - 1] != '#':
+            map.map_array[self.y][self.x] = ' '
+            self.x -= 1
+            map.map_array[self.y][self.x] = 'X'
+        return map
+
+    def move_down(self, map):
         if map.map_array[self.y + 1][self.x] != '#':
             map.map_array[self.y][self.x] = ' '
             self.y += 1
             map.map_array[self.y][self.x] = 'X'
         return map
+
+    def move_down(self, map):
+       if map.map_array[self.y - 1][self.x] != '#':
+           map.map_array[self.y][self.x] = ' '
+           self.y -= 1
+           map.map_array[self.y][self.x] = 'X'
+       return map
         
