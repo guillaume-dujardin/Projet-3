707b5c92d1f31edeed256beb1cadf08d7e280569
guillaume <guillaume.dujardin@hotmail.fr>
2019-07-09 23:14:01 +0200

    Name changed and add code in file Character.py

---
+++ Character.py
@@ -0,0 +1,24 @@
+class Character():
+    
+    def __init__(self) :
+        
+        # Character positions
+
+        self.x = 2
+        self.y = 2
+
+    def move_right(self, map):
+        if map.map_array[self.y][self.x + 1] != '#':
+            map.map_array[self.y][self.x] = ' '
+            self.x += 1
+            map.map_array[self.y][self.x] = 'X'
+            print(self.x)
+        return map
+
+    def move_left(self, map):
+        if map.map_array[self.y + 1][self.x] != '#':
+            map.map_array[self.y][self.x] = ' '
+            self.y += 1
+            map.map_array[self.y][self.x] = 'X'
+        return map
+        
