4941e9a31f73f3f5974c98587969fba3b65051bb
guillaume <guillaume.dujardin@hotmail.fr>
2019-07-09 23:12:27 +0200

    Modif main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from map import *
-from Perso import *
+#!/usr/bin/python3
+import os 
+from Map import Map
+from Character import Character
 
 #Message d'introduction au jeu
 
@@ -7,7 +9,27 @@ message_debut = "Vous incarner Mcgiver dans un labyrinthe ! Aider le à sortir i
 message_debut_modifier = message_debut.center(120,"*")
 print(message_debut_modifier)
 
+if __name__ == "__main__" :
+    
+    ## We create a map object
+    ## This object store all character useful methods and variables
 
-affiche_labyrinthe(labyrinthe) #afficher le labyrinthe
+    map = Map()
 
-print("Vous êtes X et devez rejoindre la sortie ! Bonne chance...\n")
+    map.create_map("map.txt")
+    map.display_map()
+    
+   
+
+    # We create a character objet
+    # This object store all character useful methods and variable
+    
+    character = Character()
+
+    ## Example of interaction between character and map
+    ## We move the character on map and we display the map
+    map = character.move_right(map)
+    map.display_map()
+    
+    
+    
