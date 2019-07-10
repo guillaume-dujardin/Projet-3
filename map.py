6b7a2234a0097354e702c30b076f8256dd332b82
guillaume <guillaume.dujardin@hotmail.fr>
2019-07-10 17:39:40 +0200

    Modif

--- map.py
+++ map.py
@@ -1,27 +1,23 @@
 from random import *
 
 class Map :
-    #Labyrinthe niveau 1 (plan du labyrinthe)
+
     def __init__(self) :
         self.map_array = []
-
-    # Create map with the fiven filename
-    # The created map is stored in map_arrau object variable
 
     def create_map(self,filename):
         try:
             with open(filename, "r") as map_file:
                 for line in map_file:
-                    self.map_array.append(list(line.strip()))
+                    self.map_array.append(list(line.strip()))   #Création du labyrinthe via fichier map.txt
         except FileNotFoundError:
             print("Couldn't open map file \"" + filename + "\"")
             exit()
 
-# Display map stored in Map class
 
     def display_map(self):
         for line in self.map_array:
-            for character in line:
+            for character in line:        #Affichage du labyrinthe
                 print(character, end="")
             print()
 
