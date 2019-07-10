e2441e18f763cd7a015b261152f4aa1f893fa8bb
guillaume <guillaume.dujardin@hotmail.fr>
2019-07-10 10:51:33 +0200

    Add function Victory

--- Character.py
+++ Character.py
@@ -1,3 +1,5 @@
+import os
+
 class Character():
     
     def __init__(self) :
@@ -6,6 +8,16 @@ class Character():
 
         self.x = 2
         self.y = 2
+
+    def victory(self, map):
+        if map.map_array[self.y][self.x + 1] == "O":
+            input("Voulez vous sortir du labyrinthe ? : ")
+            if "OUI" :
+                os.system('cls')
+                print("Vous sortez en vie du labyrinthe")
+                continuer_game = False
+            else :
+                continuer_game = True
 
     def move_right(self, map):
         if map.map_array[self.y][self.x + 1] != '#':
@@ -29,7 +41,7 @@ class Character():
             map.map_array[self.y][self.x] = 'X'
         return map
 
-    def move_down(self, map):
+    def move_up(self, map):
        if map.map_array[self.y - 1][self.x] != '#':
            map.map_array[self.y][self.x] = ' '
            self.y -= 1
