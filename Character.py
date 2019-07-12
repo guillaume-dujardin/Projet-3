1922b1d25a9e27a0bb98a6a399bb9372de7a813d
guillaume <guillaume.dujardin@hotmail.fr>
2019-07-12 16:00:55 +0200

    code upgrade and redundant function removal

--- Character.py
+++ Character.py
@@ -11,41 +11,21 @@ class Character():
         self.tresors = 0
         self.joueur = ""
 
-    def victory(self, map):  # Non concluant \ à modifier
+    def victory(self,map):
         if map.map_array[self.y][self.x] == "O" and self.tresors == 3 :
-            print("Gardien : Zzzz.. Zzzz.. Zzzz..  Je peut sortir du labyrinthe..")                            
-            os.system("pause")            
+            print("Gardien : Zzzz.. Zzzz.. Zzzz..  Je peut sortir du labyrinthe..")
         elif map.map_array[self.y][self.x] == "O" and self.tresors != 3 :
-            print("Vous ne passerez passsssssss !!! ")
-        else :
-            pass
+            print("Tu ne peut pas passer sans avoir tout les objets")
                 
-            
-    
     def treasure(self):
         if map.map_array[self.y][self.x + 1] == "$" :
             self.tresors += 1     
             print("Vous avez dégoter un tresor ! Cela vous en fait ",self.tresors," bravo !")
             if self.tresors == 3 :
-                print("Vous avez tout les tresors allez vite trouver le gardien pour l'endormir !!")
-        if map.map_array[self.y][self.x - 1] == "$" :
-            self.tresors += 1     
-            print("Vous avez dégoter un tresor ! Cela vous en fait ",self.tresors," bravo !")
-            if self.tresors == 3 :
-                print("Vous avez tout les tresors allez vite trouver le gardien pour l'endormir !!")
-        if map.map_array[self.y + 1][self.x] == "$" :
-            self.tresors += 1     
-            print("Vous avez dégoter un tresor ! Cela vous en fait ",self.tresors," bravo !")
-            if self.tresors == 3 :
-                print("Vous avez tout les tresors allez vite trouver le gardien pour l'endormir !!")
-        if map.map_array[self.y - 1][self.x] == "$" :
-            self.tresors += 1     
-            print("Vous avez dégoter un tresor ! Cela vous en fait ",self.tresors," bravo !")
-            if self.tresors == 3 :
-                print("Vous avez tout les tresors allez vite trouver le gardien pour l'endormir !!")
-
-    def check_treasure(self, map, pos_x, pos_y):
-        if map.map_array[pos_y][pos_x] == "$" :
+                print("Tout les objets sont en votre possession vite go chez le gardien pour le faire dodo")
+        
+    def check_treasure(self, map, x, y):
+        if map.map_array[y][x] == "$" :
             self.tresors += 1
             print("Vous avez dégoter un tresor ! Cela vous en fait ",self.tresors," bravo !")
             if self.tresors == 3 :
@@ -76,7 +56,6 @@ class Character():
             
 
     def move_right(self, map):
-        self.victory(map)
         if map.map_array[self.y][self.x + 1] == "$" :
             self.tresors += 1     
             print("Vous avez dégoter un tresor ! Cela vous en fait ",self.tresors," bravo !")
@@ -113,6 +92,8 @@ class Character():
             self.move_d(map)
         elif map.map_array[self.y + 1][self.x] != '#':
             self.move_d(map)
+        else :
+            print("Aie ma tête.. Qui à eu l'idée de placé un mur ici..'")
         return map
 
     def move_up(self, map):
@@ -125,6 +106,8 @@ class Character():
             self.move_u(map)
         elif map.map_array[self.y - 1][self.x] != '#':
             self.move_u(map)
+        else :
+            print("Aie ma tête.. Qui à eu l'idée de placé un mur ici..'")
         return map
 
 
