026cfac87693f3ebf4003686fb033a380b531ed4
guillaume <guillaume.dujardin@hotmail.fr>
2019-07-14 20:09:03 +0200

    Delete last code (victory) and save

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ class Game :
     def jouer(self):
         if __name__ == "__main__" :
 
+            self.y = 1
+            self.x = 1
             message_debut = "Vous incarner Mcgiver dans un labyrinthe ! Aider le à sortir indemne"
             message_debut_modifier = message_debut.center(120,"*")
             print(message_debut_modifier)
@@ -53,23 +55,26 @@ class Game :
                 else:
                     print("On avancera jamais avec",player)
                 
-                if character.victory(map) :
-                    player = input("Voulez vous rejouer ? : ")
-                    if player == "OUI" :
-                        continuer_game = True
-                    elif player == "NON" :
-                        continuer_game = False
-                        if continuer_game == False :
-                            print("Vous arrêter de jouer")
-                            exit()
-                    else : print("C'est OUI ou NON")
+            quitter = input("Voulez vous quitter ? : (OUI,NON)")
+            if quitter == "OUI" :
+                print("Vous arreter de jouer")
+                continuer_game = False
+                if continuer_game == False :
+                    exit()
+            elif quitter == "NON" :
+                continuer_game = True
+
 
 # Ajouter 3 tresors aleatoirement dans le labyrinthe a chaque début de jeu
-# resoudre le problème du "O" quand on finit la partie
 # Permettre au joueur de recommencer la partie
-# faire en sorte que le personnage ne puisse pas passez si il n'a pas tout les objets
 # Donner un nom aux objets different ramasser dans le labyrinthe
 # Recuperer la saisi utilisateur sur les touches directionnel pour directement faire une action
+
+#alors tranchons pour map.py, on n'utilise pas de majuscule dans les noms de modules
+#Pour le placement aléatoire, tu peux p.ex. utiliser random.sample() directement avec
+# ta liste de positions des chemins. Cela tirera au sort des positions sans répétition 
+# dans ta liste. Si tu n'as pas de liste de positions des chemins, tu peux en construire 
+# une à la lecture du fichier décrivant la structure de labyrinthe.
                     
                 
         
