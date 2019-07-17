9d437878b80492eca3b8a046514f818daaa3bb55
guillaume <guillaume.dujardin@hotmail.fr>
2019-07-17 16:06:29 +0200

    Add code

--- map.py
+++ map.py
@@ -1,24 +1,30 @@
 from random import *
 
 class Map :
-
+    
     def __init__(self) :
         self.map_array = []
+        self.case_libre = []
 
     def create_map(self,filename):
         try:
             with open(filename, "r") as map_file:
                 for line in map_file:
                     self.map_array.append(list(line.strip()))   #Création du labyrinthe via fichier map.txt
+                    for character in line :
+                        if character == " " :
+                            self.case_libre.append(list)
         except FileNotFoundError:
             print("Couldn't open map file \"" + filename + "\"")
             exit()
-
+    
+    def display_objet(self):
+        print()
 
     def display_map(self):
         for line in self.map_array:            
             for character in line:        #Affichage du labyrinthe
-                print(character, end="")
+                print(character, end="")                
             print()
 
     def is_passage(self, x, y):
