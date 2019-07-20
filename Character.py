b1b7eaa15c5d620e461937cb73a4ca6bc2f4ddc3
guillaume <guillaume.dujardin@hotmail.fr>
2019-07-20 14:44:24 +0200

    Add code

--- Character.py
+++ Character.py
@@ -4,11 +4,19 @@ from Map import Map
 class Character():
     
     def __init__(self) :
-        
+      
         self.x = 1
         self.y = 1
         self.tresors = 0
         self.joueur = ""
+        self.pos_ob = []
+
+    def objet(self) :
+        for ligne in map.map_array :
+            for colonne in ligne :
+                if self.x and self.y == " " :
+                    self.pos_ob.append(self.x,self.y)
+
 
     def victory(self,map,pos_y,pos_x):
         if map.map_array[pos_y][pos_x] == "O" and self.tresors == 3 :
@@ -52,35 +60,32 @@ class Character():
         elif map.map_array[self.y][self.x + 1] != '#':
             self.move_r(map)
         else :
-            print("Aie ma tête.. Qui à eu l'idée de placé un mur ici..'")
+            print("Aie ma tête.. Qui a eu l'idée de placer un mur ici...'")
         return map
 
     def move_left(self, map):
-        self.victory(map,self.y,self.x - 1)
         if self.check_treasure(map,self.y,self.x - 1) == True :
             self.move_l(map)
         elif map.map_array[self.y][self.x - 1] != '#':
             self.move_l(map)
         else :
-            print("Aie ma tête.. Qui à eu l'idée de placé un mur ici..'")
+            print("Aie ma tête.. Qui a eu l'idée de placer un mur ici...'")
         return map
 
     def move_down(self, map):
-        self.victory(map,self.y + 1,self.x)
         if self.check_treasure(map,self.y + 1,self.x) == True :
             self.move_d(map)
         elif map.map_array[self.y + 1][self.x] != '#':
             self.move_d(map)
         else :
-            print("Aie ma tête.. Qui à eu l'idée de placé un mur ici..'")
+            print("Aie ma tête.. Qui a eu l'idée de placer un mur ici...'")
         return map
 
     def move_up(self, map):
-        self.victory(map,self.y - 1,self.x)
         if self.check_treasure(map,self.y - 1,self.x) == True :
             self.move_u(map)
         elif map.map_array[self.y - 1][self.x] != '#':
             self.move_u(map)
         else :
-            print("Aie ma tête.. Qui à eu l'idée de placé un mur ici..'")
+            print("Aie ma tête.. Qui a eu l'idée de placer un mur ici...'")
         return map
