b0611ff50046eca8a7d41b25a596ba1b33333bde
guillaume <guillaume.dujardin@hotmail.fr>
2019-07-20 14:44:35 +0200

    Add code

--- Game.py
+++ Game.py
@@ -8,65 +8,64 @@ class Game :
     def __init__(self):
         pass
 
-    def jouer(self):        
-            map = Map()
-            character = Character()
+    def init_game(self):
+        self.character = Character()
+        self.map = Map()        
+        self.map.create_map("map.txt")   # CrÃ©ation de la carte
+        
+
+    def jouer(self):            
+            self.init_game()
             continuer_game = 'O'
             while continuer_game != 'N' :
-                print(map.case_libre)                
                 player = ''
                 partie = True                
                 message_debut = "Vous incarner Mcgiver dans un labyrinthe ! Aider le a sortir indemne"
                 message_debut_modifier = message_debut.center(120,"*")
                 print(message_debut_modifier)
-                map.create_map("map.txt")   # Création de la carte
-                map.display_map()   # Affichage de la carte
+                
+                self.map.display_map()   # Affichage de la carte
                 
                 while partie == True :
                     player = input("Voulez vous allez en bas en haut a droite a gauche ? : ")                    
-                    if player == "R" :
-                        os.system('cls')
-                        map = character.move_right(map)    
-                        map.display_map()
-                        if map.map_array[character.y][character.x + 1] == "O" and character.tresors != 3 :
+                    if player == "R" :                        
+                        self.map = self.character.move_right(self.map)                            
+                        if self.map.map_array[self.character.y][self.character.x + 1] == "O" and self.character.tresors != 3 :
                             print("Tu ne peut pas passer sans avoir tout les objets")
-                        elif map.map_array[character.y][character.x + 1] == "O" and character.tresors == 3 :
+                        elif self.map.map_array[self.character.y][self.character.x + 1] == "O" and self.character.tresors == 3 :
                             print("Gardien : Zzzz.. Zzzz.. Zzzz..  Je peut sortir du labyrinthe..")
                             partie = False
                             
                     elif player == "L" :
-                        os.system('cls')
-                        map = character.move_left(map)    
-                        map.display_map()
-                        if map.map_array[character.y][character.x - 1] == "O" and character.tresors != 3 :
+                        self.map = self.character.move_left(self.map)
+                        if self.map.map_array[self.character.y][self.character.x - 1] == "O" and self.character.tresors != 3 :
                             print("Tu ne peut pas passer sans avoir tout les objets")
-                        elif map.map_array[character.y][character.x - 1] == "O" and character.tresors == 3 :
+                        elif self.map.map_array[self.character.y][self.character.x - 1] == "O" and self.character.tresors == 3 :
                             print("Gardien : Zzzz.. Zzzz.. Zzzz..  Je peut sortir du labyrinthe..")
                             partie = False
                     elif player == "D" :
-                        os.system('cls')
-                        map = character.move_down(map) 
-                        map.display_map()
-                        if map.map_array[character.y + 1][character.x] == "O" and character.tresors != 3 :
+                        self.map = self.character.move_down(self.map)
+                        if self.map.map_array[self.character.y + 1][self.character.x] == "O" and self.character.tresors != 3 :
                             print("Tu ne peut pas passer sans avoir tout les objets")
-                        elif map.map_array[character.y + 1][character.x] == "O" and character.tresors == 3 :
+                        elif self.map.map_array[self.character.y + 1][self.character.x] == "O" and self.character.tresors == 3 :
                             print("Gardien : Zzzz.. Zzzz.. Zzzz..  Je peut sortir du labyrinthe..")
                             partie = False
                     elif player == "U" :
-                        os.system('cls')
-                        map = character.move_up(map)    
-                        map.display_map()
-                        if map.map_array[character.y - 1][character.x] == "O" and character.tresors != 3 :
+                        self.map = self.character.move_up(self.map)    
+                        if self.map.map_array[self.character.y - 1][self.character.x] == "O" and self.character.tresors != 3 :
                             print("Tu ne peut pas passer sans avoir tout les objets")
-                        elif map.map_array[character.y - 1][character.x] == "O" and character.tresors == 3 :
+                        elif self.map.map_array[self.character.y - 1][self.character.x] == "O" and self.character.tresors == 3 :
                             print("Gardien : Zzzz.. Zzzz.. Zzzz..  Je peut sortir du labyrinthe..")
                             partie = False
-                    else:
-                        print("On avancera jamais avec",player)
+
+                    os.system('cls')
+                    self.map.display_map()
+
                 while partie == False :
                     continuer_game = input("Voulez vous recommencer la partie (OUI/NON) ? : ") 
                     if continuer_game == "OUI" :                        
                         partie = True
+                        self.init_game()
                         os.system('cls')
                     elif continuer_game == "NON" :
                         continuer_game = False
