08ccdae5437d7d0ba9166b7369e40a9be84425a0
guillaume <guillaume.dujardin@hotmail.fr>
2019-07-21 22:19:55 +0200

    modif victory, opti functions

--- Character.py
+++ Character.py
@@ -5,28 +5,17 @@ class Character():
     
     def __init__(self) :
       
-        self.x = 1
-        self.y = 1
+        self.line = 1
+        self.column = 1
         self.tresors = 0
         self.joueur = ""
-        self.pos_ob = []
-
-    def objet(self) :
-        for ligne in map.map_array :
-            for colonne in ligne :
-                if self.x and self.y == " " :
-                    self.pos_ob.append(self.x,self.y)
-
-
-    def victory(self,map,pos_y,pos_x):
-        if map.map_array[pos_y][pos_x] == "O" and self.tresors == 3 :
-            print("Gardien : Zzzz.. Zzzz.. Zzzz..  Je peut sortir du labyrinthe..")            
-        elif map.map_array[pos_y][pos_x] == "O" and self.tresors != 3 :
-            print("Tu ne peut pas passer sans avoir tout les objets")
+        
+    def victory(self,map,pos_column,pos_line):            
+        if map.map_array[pos_column][pos_line] == "O" and self.tresors != 3 :
             return True
 
-    def check_treasure(self, map,pos_y,pos_x):
-        if map.map_array[pos_y][pos_x] == "$" :
+    def check_treasure(self, map,pos_column,pos_line):
+        if map.map_array[pos_column][pos_line] == "$" :
             self.tresors += 1
             print("Vous avez dégoter un tresor ! Cela vous en fait ",self.tresors," bravo !")
             if self.tresors == 3 :
@@ -35,56 +24,64 @@ class Character():
         return False
 
     def move_r(self, map):
-        map.map_array[self.y][self.x] = ' '
-        self.x += 1
-        map.map_array[self.y][self.x] = 'X'
+        map.map_array[self.column][self.line] = ' '
+        self.line += 1
+        map.map_array[self.column][self.line] = 'X'
 
     def move_l(self, map):
-        map.map_array[self.y][self.x] = ' '
-        self.x -= 1
-        map.map_array[self.y][self.x] = 'X'
+        map.map_array[self.column][self.line] = ' '
+        self.line -= 1
+        map.map_array[self.column][self.line] = 'X'
 
     def move_u(self, map):
-        map.map_array[self.y][self.x] = ' '
-        self.y -= 1
-        map.map_array[self.y][self.x] = 'X'
+        map.map_array[self.column][self.line] = ' '
+        self.column -= 1
+        map.map_array[self.column][self.line] = 'X'
 
     def move_d(self, map):
-        map.map_array[self.y][self.x] = ' '
-        self.y += 1
-        map.map_array[self.y][self.x] = 'X'
+        map.map_array[self.column][self.line] = ' '
+        self.column += 1
+        map.map_array[self.column][self.line] = 'X'
      
     def move_right(self, map):  
-        if self.check_treasure(map,self.y,self.x + 1) == True :
+        if self.check_treasure(map,self.column,self.line + 1) == True :
             self.move_r(map)
-        elif map.map_array[self.y][self.x + 1] != '#':
+        elif self.victory(map,self.column,self.line +1) :
+            print("tu ne peut pas passer sans tout les objets")
+        elif map.map_array[self.column][self.line + 1] != '#':
             self.move_r(map)
         else :
             print("Aie ma tête.. Qui a eu l'idée de placer un mur ici...'")
         return map
 
     def move_left(self, map):
-        if self.check_treasure(map,self.y,self.x - 1) == True :
+        if self.check_treasure(map,self.column,self.line - 1) == True :
             self.move_l(map)
-        elif map.map_array[self.y][self.x - 1] != '#':
+        elif self.victory(map,self.column,self.line - 1) :
+            print("tu ne peut pas passer sans tout les objets")
+        elif map.map_array[self.column][self.line - 1] != '#':
             self.move_l(map)
         else :
             print("Aie ma tête.. Qui a eu l'idée de placer un mur ici...'")
         return map
 
     def move_down(self, map):
-        if self.check_treasure(map,self.y + 1,self.x) == True :
+        if self.check_treasure(map,self.column + 1,self.line) == True :
             self.move_d(map)
-        elif map.map_array[self.y + 1][self.x] != '#':
+        elif self.victory(map,self.column + 1,self.line) :
+            print("tu ne peut pas passer sans tout les objets")
+        elif map.map_array[self.column + 1][self.line] != '#':
             self.move_d(map)
         else :
             print("Aie ma tête.. Qui a eu l'idée de placer un mur ici...'")
         return map
 
     def move_up(self, map):
-        if self.check_treasure(map,self.y - 1,self.x) == True :
+        if self.check_treasure(map,self.column - 1,self.line) == True :
             self.move_u(map)
-        elif map.map_array[self.y - 1][self.x] != '#':
+        elif self.victory(map,self.column - 1,self.line) :
+            print("tu ne peut pas passer sans tout les objets")
+        elif map.map_array[self.column - 1][self.line] != '#':
             self.move_u(map)
         else :
             print("Aie ma tête.. Qui a eu l'idée de placer un mur ici...'")
