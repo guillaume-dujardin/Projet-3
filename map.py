ec62037f98c213b13d9f17b2a80238fb1ee42d37
guillaume <guillaume.dujardin@hotmail.fr>
2019-07-21 22:22:31 +0200

    add functions path_map_finder and pos_objet

--- map.py
+++ map.py
@@ -1,30 +1,43 @@
-from random import *
+from random import sample
 
 class Map :
     
     def __init__(self) :
         self.map_array = []
-        self.pos_ob = []
+        self.lab = []
 
     def create_map(self,filename):
         try:
             with open(filename, "r") as map_file:
                 for line in map_file:
-                    self.map_array.append(list(line.strip()))   #Création du labyrinthe via fichier map.txt                    
+                    self.map_array.append(list(line.strip()))                
                         
         except FileNotFoundError:
             print("Couldn't open map file \"" + filename + "\"")
             exit()    
+    
 
+    def path_map_finder(self) :
+        for y, line in enumerate(self.map_array) :
+            for x, character in enumerate(line) :
+                if character == " " :
+                    self.lab.append((x,y))
+    
+    def pos_objet(self):
+        i = 0
+        self.path_map_finder()
+        while i < 3 :
+            pos = sample(self.lab,1)
+            self.map_array[pos] = "$"
            
     
     def display_map(self):
         i = 0
         for line in self.map_array:            
-            for character in line:        #Affichage du labyrinthe
+            for character in line:
                 print(character, end="")                
             print()
-    
+
     
 
 
