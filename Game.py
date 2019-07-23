c67dd04828bc2089160bd25dfc0c1f787b1f3101
guillaume <guillaume.dujardin@hotmail.fr>
2019-07-23 14:05:43 +0200

    add pygame

--- Game.py
+++ Game.py
@@ -2,6 +2,11 @@ import os
 from Map import Map
 from Character import Character
 import time
+import pygame
+from pygame.locals import *
+from Constante import *
+
+
 
 class Game :
 
@@ -12,42 +17,57 @@ class Game :
         self.character = Character()
         self.map = Map()        
         self.map.create_map("map.txt")
+        pygame.init()
+
         
-    def play(self):            
+    def play(self):      
+        
             self.init_game()
-            continuer_game = 'O'
-            while continuer_game != 'N' :
-                player = ''
-                partie = True                
-                message_debut = "Vous incarner Mcgiver dans un labyrinthe ! Aider le a sortir indemne"
-                message_debut_modifier = message_debut.center(120,"*")
-                print(message_debut_modifier)
+            window = pygame.display.set_mode((400,400)) # Affichage de la fenetre
+
+            icon = pygame.image.load(picture_Mc) # Chargement de l'icone du joueur
+            pygame.display.set_icon(icon)
+
+            pygame.display.set_caption(picture_home)
+
+            continue_game = 'O'
+            while continue_game != 'N' :
+                fond = pygame.image.load(picture_home).convert()
+                game = True                
+                start_message = "Vous incarner Mcgiver dans un labyrinthe ! Aider le a sortir indemne"
+                start_message_edit = start_message.center(120,"*")
+                print(start_message_edit)
                 
                 self.map.display_map()
                 
-                while partie == True :
+                while game == True :
                     
-                    player = input("Voulez vous allez en bas en haut a droite a gauche ? : ")                    
-                    if player == "R" or player == "r" :                       
-                        self.map = self.character.move_right(self.map)                            
-                        if self.map.map_array[self.character.column][self.character.line + 1] == "O" and self.character.tresors == 3 :
-                            print("Gardien : Zzzz.. Zzzz.. Zzzz..  Je peut sortir du labyrinthe..")
-                            partie = False                         
-                    elif player == "L" or player == "l" :
-                        self.map = self.character.move_left(self.map)
-                        if self.map.map_array[self.character.column][self.character.line - 1] == "O" and self.character.tresors == 3 :
-                            print("Gardien : Zzzz.. Zzzz.. Zzzz..  Je peut sortir du labyrinthe..")
-                            partie = False
-                    elif player == "D" or player == "d" :
-                        self.map = self.character.move_down(self.map)
-                        if self.map.map_array[self.character.column + 1][self.character.line] == "O" and self.character.tresors == 3 :
-                            print("Gardien : Zzzz.. Zzzz.. Zzzz..  Je peut sortir du labyrinthe..")
-                            partie = False
-                    elif player == "U" or player == "u" :
-                        self.map = self.character.move_up(self.map)    
-                        if self.map.map_array[self.character.column - 1][self.character.line] == "O" and self.character.tresors == 3 :
-                            print("Gardien : Zzzz.. Zzzz.. Zzzz..  Je peut sortir du labyrinthe..")
-                            partie = False
+                    for event in pygame.event.get() :
+                        
+                        if event.type == QUIT :
+                            exit()
+
+                        elif event.type == KEYDOWN :
+
+                            if event.key == K_ESCAPE :
+                                exit()
+
+                            elif event.type == K_RIGHT :                       
+                                self.map = self.character.move_right(self.map)                            
+                                if self.map.map_array[self.character.column][self.character.line + 1] == "O" and self.character.tresors == 3 :                                
+                                    game = False                         
+                            elif event.type == K_LEFT :
+                                self.map = self.character.move_left(self.map)
+                                if self.map.map_array[self.character.column][self.character.line - 1] == "O" and self.character.tresors == 3 :
+                                    game = False
+                            elif event.type == K_DOWN :
+                                self.map = self.character.move_down(self.map)
+                                if self.map.map_array[self.character.column + 1][self.character.line] == "O" and self.character.tresors == 3 :                               
+                                    game = False
+                            elif event.type == K_UP :
+                                self.map = self.character.move_up(self.map)    
+                                if self.map.map_array[self.character.column - 1][self.character.line] == "O" and self.character.tresors == 3 :                                
+                                    game = False
                     
                     
                     os.system('cls')
@@ -55,14 +75,14 @@ class Game :
                     
                     
 
-                while partie == False :
-                    continuer_game = input("Voulez vous recommencer la partie (OUI/NON) ? : ") 
-                    if continuer_game == "OUI" or continuer_game == "oui" :                       
-                        partie = True
+                while game == False :
+                    continue_game = input("Voulez vous recommencer la partie (OUI/NON) ? : ") 
+                    if continue_game == "OUI" or continue_game == "oui" :                       
+                        game = True
                         self.init_game()
                         os.system('cls')
-                    elif continuer_game == "NON" or continuer_game == "non" :
+                    elif continue_game == "NON" or continue_game == "non" :
                         exit()
                     else :
-                        print("C'est OUI ou NON")
+                        continue
 
