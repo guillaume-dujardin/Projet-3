ef89db096bf0a405575e97383350c629545c6e4b
guillaume <guillaume.dujardin@hotmail.fr>
2019-07-24 09:19:54 +0200

    add code

--- Game.py
+++ Game.py
@@ -2,9 +2,7 @@ import os
 from Map import Map
 from Character import Character
 import time
-import pygame
-from pygame.locals import *
-from Constante import *
+from Constant import *
 
 
 
@@ -17,22 +15,13 @@ class Game :
         self.character = Character()
         self.map = Map()        
         self.map.create_map("map.txt")
-        pygame.init()
 
         
     def play(self):      
-        
+           
             self.init_game()
-            window = pygame.display.set_mode((400,400)) # Affichage de la fenetre
-
-            icon = pygame.image.load(picture_Mc) # Chargement de l'icone du joueur
-            pygame.display.set_icon(icon)
-
-            pygame.display.set_caption(picture_home)
-
             continue_game = 'O'
-            while continue_game != 'N' :
-                fond = pygame.image.load(picture_home).convert()
+            while continue_game != 'N' :                
                 game = True                
                 start_message = "Vous incarner Mcgiver dans un labyrinthe ! Aider le a sortir indemne"
                 start_message_edit = start_message.center(120,"*")
@@ -41,33 +30,25 @@ class Game :
                 self.map.display_map()
                 
                 while game == True :
-                    
-                    for event in pygame.event.get() :
-                        
-                        if event.type == QUIT :
-                            exit()
+                    player = ""
+                    player = input("L,D,R,U : ")
 
-                        elif event.type == KEYDOWN :
-
-                            if event.key == K_ESCAPE :
-                                exit()
-
-                            elif event.type == K_RIGHT :                       
-                                self.map = self.character.move_right(self.map)                            
-                                if self.map.map_array[self.character.column][self.character.line + 1] == "O" and self.character.tresors == 3 :                                
-                                    game = False                         
-                            elif event.type == K_LEFT :
-                                self.map = self.character.move_left(self.map)
-                                if self.map.map_array[self.character.column][self.character.line - 1] == "O" and self.character.tresors == 3 :
-                                    game = False
-                            elif event.type == K_DOWN :
-                                self.map = self.character.move_down(self.map)
-                                if self.map.map_array[self.character.column + 1][self.character.line] == "O" and self.character.tresors == 3 :                               
-                                    game = False
-                            elif event.type == K_UP :
-                                self.map = self.character.move_up(self.map)    
-                                if self.map.map_array[self.character.column - 1][self.character.line] == "O" and self.character.tresors == 3 :                                
-                                    game = False
+                    if player == "R" :                       
+                        self.map = self.character.move_right(self.map)                            
+                        if self.map.map_array[self.character.column][self.character.line + 1] == "O" and self.character.tresors == 3 :                                
+                            game = False                         
+                    elif player == "L" :
+                        self.map = self.character.move_left(self.map)
+                        if self.map.map_array[self.character.column][self.character.line - 1] == "O" and self.character.tresors == 3 :
+                            game = False
+                    elif player == "D" :
+                        self.map = self.character.move_down(self.map)
+                        if self.map.map_array[self.character.column + 1][self.character.line] == "O" and self.character.tresors == 3 :                               
+                            game = False
+                    elif player == "U" :
+                        self.map = self.character.move_up(self.map)    
+                        if self.map.map_array[self.character.column - 1][self.character.line] == "O" and self.character.tresors == 3 :                                
+                            game = False
                     
                     
                     os.system('cls')
