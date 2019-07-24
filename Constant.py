9a41bd6a516515912a2e219d543fe300ccfd807d
guillaume <guillaume.dujardin@hotmail.fr>
2019-07-24 09:19:23 +0200

    add code

--- Constant.py
+++ Constant.py
@@ -1,5 +1,9 @@
-picture_background = "images/decor.jpg"
-picture_home = "images/accueil.jpg"
-picture_Mc = "images/MacGyver.png"
-picture_guardian = "images/Gardien.png"
-picture_wall = "images/mur.jpg"
+nombre_sprite_cote = 15
+taille_sprite = 30
+cote_fenetre = nombre_sprite_cote * taille_sprite
+
+PICTURE_BACKGROUND = "images/decor.jpg"
+PICTURE_HOME = "images/accueil.jpg"
+PICTURE_MC = "images/MacGyver.png"
+PICTURE_GUARDIAN = "images/Gardien.png"
+PICTURE_WALL = "images/mur.jpg"
