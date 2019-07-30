94dd33ba692c2d891ab2c372bb2dcdcfaa5ef11e
guillaume <guillaume.dujardin@hotmail.fr>
2019-07-30 12:46:50 +0200

    code in english

--- Constant.py
+++ Constant.py
@@ -4,10 +4,10 @@ side_window = number_of_square * square_size
 
 IMAGE_ICONE = "ressources/MacGyver.png" # IMAGE_ICONE = the location of the photo
 
-IMAGE_FOND = "ressources/background.png"
+IMAGE_BACKGROUND = "ressources/background.png"
 IMAGE_WALL = "ressources/wall.gif"
 
 IMAGE_ETHER = "ressources/ether.png"
-IMAGE_AIGUILLE = "ressources/needle.jpg"
-IMAGE_TUBE_PLASTIQUE = "ressources/plastic_tube.png"
-IMAGE_GARDIEN = "ressources/Guardian.png"
+IMAGE_NEEDLE = "ressources/needle.jpg"
+IMAGE_PLASTIC_TUBE = "ressources/plastic_tube.png"
+IMAGE_GUARDIAN = "ressources/Guardian.png"
