a08e18ac7830c6eed33c6f4ee94dffb055069d03
guillaume <guillaume.dujardin@hotmail.fr>
2019-07-30 22:29:23 +0200

    Add and modif function

--- Constant.py
+++ Constant.py
@@ -2,11 +2,10 @@ number_of_square = 15
 square_size = 30
 side_window = number_of_square * square_size
 
+IMAGE_HOME = "ressources/home.jpg"
 IMAGE_ICONE = "ressources/MacGyver.png" # IMAGE_ICONE = the location of the photo
-
 IMAGE_BACKGROUND = "ressources/background.png"
 IMAGE_WALL = "ressources/wall.gif"
-
 IMAGE_ETHER = "ressources/ether.png"
 IMAGE_NEEDLE = "ressources/needle.jpg"
 IMAGE_PLASTIC_TUBE = "ressources/plastic_tube.png"
