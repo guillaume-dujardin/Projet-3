956cef4da4f8887a7138979f61736e2899f1df2b
guillaume <guillaume.dujardin@hotmail.fr>
2019-07-31 20:45:03 +0200

    Add function

--- Constant.py
+++ Constant.py
@@ -2,6 +2,10 @@ number_of_square = 15
 square_size = 30
 side_window = number_of_square * square_size
 
+IMAGE_FIRST_HOME = "ressources/first_home.jpg"
+IMAGE_PLAY_BUTTON = "ressources/play_button.png"
+IMAGE_BLUE_BUTTON = "ressources/Bleu_button.png"
+IMAGE_RULE_OF_THE_GAME = "ressources/Rule_of_the_game.png"
 IMAGE_HOME = "ressources/home.jpg"
 IMAGE_ICONE = "ressources/MacGyver.png" # IMAGE_ICONE = the location of the photo
 IMAGE_BACKGROUND = "ressources/background.png"
