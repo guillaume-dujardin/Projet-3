9859f0be6edb7daecf0412f01ee226a29e58c092
guillaume <guillaume.dujardin@hotmail.fr>
2019-08-03 20:43:05 +0200

    Modif display pygame

--- Constant.py
+++ Constant.py
@@ -1,13 +1,15 @@
 number_of_square = 15
 square_size = 30
 side_window = number_of_square * square_size
-
+# Image
 IMAGE_FIRST_HOME = "ressources/first_home.jpg"
-IMAGE_PLAY_BUTTON = "ressources/play_button.png"
-IMAGE_BLUE_BUTTON = "ressources/Bleu_button.png"
-IMAGE_RULE_OF_THE_GAME = "ressources/Rule_of_the_game.png"
+IMAGE_PLAY_BUTTON = "ressources/play_button.jpg"
+IMAGE_RULES_BUTTON = "ressources/rule_button.png"
+IMAGE_EXIT_BUTTON = "ressources/exit_button.png"
+IMAGE_RETURN_BUTTON = "ressources/return_button.png"
+IMAGE_RULE_OF_THE_GAME = "ressources/Rule_game.png"
 IMAGE_HOME = "ressources/home.jpg"
-IMAGE_ICONE = "ressources/MacGyver.png" # IMAGE_ICONE = the location of the photo
+IMAGE_ICONE = "ressources/MacGyver.png"
 IMAGE_BACKGROUND = "ressources/background.png"
 IMAGE_WALL = "ressources/wall.png"
 IMAGE_ETHER = "ressources/ether.png"
