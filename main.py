5674f570a340aac88fea1e582aac24a150f4367c
guillaume <guillaume.dujardin@hotmail.fr>
2019-08-04 15:50:01 +0200

    Delete comment

--- main.py
+++ main.py
@@ -2,17 +2,6 @@
 
 from Game import Game
 
-# Ajouter 3 tresors aleatoirement dans le labyrinthe a chaque debut de jeu
-# Permettre au joueur de recommencer la partie
-# Donner un nom aux objets different ramasser dans le labyrinthe
-# Recuperer la saisi utilisateur sur les touches directionnel pour directement faire une action
-
-#alors tranchons pour map.py, on n'utilise pas de majuscule dans les noms de modules
-# Pour le placement aleatoire, tu peux peux. utiliser random.sample() directement avec
-# ta liste de positions des chemins. Cela tirera au sort des positions sans repetition 
-# dans ta liste. Si tu n'as pas de liste de positions des chemins, tu peux en construire 
-# une a la lecture du fichier decrivant la structure de labyrinthe.
-
 def launching(): # function to start the game
     first_game = Game()
     first_game.play()
